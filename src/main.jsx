631ee617beeb07e141b40ce997c36ec48af2d814
Sohel Khan <khansohel7744@gmail.com>
2026-03-11 16:53:00 +0530

    added again

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css"
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <HashRouter  basename="/mamarogyam-website">
     <App />
   </HashRouter>
 );
