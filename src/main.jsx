4de413cdb344fa08414f6f73147e997d04480c5a
Sohel Khan <khansohel7744@gmail.com>
2026-03-11 16:55:38 +0530

    Added this

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-
-import App from "./App"
-import "./index.css"
-import { HashRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter  basename="/mamarogyam-website">
+  <BrowserRouter basename="/mamarogyam-website">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
